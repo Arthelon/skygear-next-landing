5d85351afafaa5d1543c339fcb2f6a87345b2751
Frank Lam <lkhfrank@gmail.com>
2019-07-24 18:36:25 +0800

    Refine nav menu, add mobile menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,17 @@ $(document).foundation();
 //
 // Custom JS
 // --------------------------------------------------
+
+$(document).ready(function(){
+
+	var $win = $(window);
+
+     $win.scroll(function () {
+         if ($win.scrollTop() <= 0)
+            $('#MainHeader').removeClass('scr');
+         else {
+         	 $('#MainHeader').addClass('scr');
+         }
+     });
+
+});
